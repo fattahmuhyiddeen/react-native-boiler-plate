410da642d32b33f87aa75e20f761ea8a6c555fb5
fattahmuhyiddeen <fattahmuhyiddeen@gmail.com>
2019-12-30 19:36:19 +0800

    bug fix

--- src/route/NavigationService.js
+++ src/route/NavigationService.js
@@ -2,22 +2,10 @@ import { NavigationActions } from 'react-navigation';
 
 let _navigator;
 
-function setTopLevelNavigator(navigatorRef) {
-  _navigator = navigatorRef;
-}
+export const setTopLevelNavigator = navigatorRef => _navigator = navigatorRef;
 
-function navigate(routeName, params) {
+export const navigate = (routeName, params) =>
   _navigator.dispatch(NavigationActions.navigate({ routeName, params }));
-}
 
-function goBack() {
+export const goBack = () =>
   _navigator.dispatch(NavigationActions.back());
-}
-
-// add other navigation functions that you need and export them
-
-export default {
-  navigate,
-  setTopLevelNavigator,
-  goBack,
-};
