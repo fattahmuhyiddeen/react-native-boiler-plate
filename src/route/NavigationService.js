688637aba2df5e1373e417be3279cca257af583c
Fattah Muhyiddeen <fattahmuhyiddeen@gmail.com>
2020-03-24 00:26:44 +0800

    bug fix

--- src/route/NavigationService.js
+++ src/route/NavigationService.js
@@ -1,15 +1,3 @@
-// import { NavigationActions } from 'react-navigation';
-
-// let _navigator;
-
-// export const setTopLevelNavigator = navigatorRef => _navigator = navigatorRef;
-
-// export const navigate = (routeName, params) =>
-//   _navigator.dispatch(NavigationActions.navigate({ routeName, params }));
-
-// export const goBack = () =>
-//   _navigator.dispatch(NavigationActions.back());
-
 import { createRef } from 'react';
 
 export const isMountedRef = createRef();
