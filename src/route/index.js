17abd9d996c1766a81e7d70ed4912646bcec73a2
Fattah Muhyiddeen <fattahmuhyiddeen@gmail.com>
2020-03-24 00:35:54 +0800

    bug fix

--- src/route/index.js
+++ src/route/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
+import Home from '@screens/home';
 
 const Stack = createStackNavigator();
 
 export const AuthNav = () => (
   <Stack.Navigator>
-    <Stack.Screen options={{ headerShown: false }} name="Home" component={() => null} />
+    <Stack.Screen options={{ headerShown: false }} name="Home" component={Home} />
   </Stack.Navigator>
 )
