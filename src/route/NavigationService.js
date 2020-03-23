7b3dc7827b19dd7cfa26c37c29dd70be849607a0
Fattah Muhyiddeen <fattahmuhyiddeen@gmail.com>
2020-03-24 00:26:29 +0800

    bug fix

--- src/route/NavigationService.js
+++ src/route/NavigationService.js
@@ -25,3 +25,13 @@ export const navigate = (name, params) => {
     // You can ignore this, or add these actions to a queue you can call later
   }
 }
+
+export const back = () => {
+  if (isMountedRef.current && navigationRef.current) {
+    // Perform navigation if the app has mounted
+    navigationRef.current.back();
+  } else {
+    // You can decide what to do if the app hasn't mounted
+    // You can ignore this, or add these actions to a queue you can call later
+  }
+}
